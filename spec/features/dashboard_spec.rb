a9ffb5105cb100c2a335f97844a18152da14902c
Tiago Pereira <tiago@glin.com.br>
2021-06-14 19:36:55 -0300

    dashboard test if index books

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -5,4 +5,9 @@ feature "Dashboards", type: :feature do
     visit(root_path)
     expect(page).to have_content('Bem-Vindo')
   end
+
+  scenario 'Verificar exibição de livro' do
+    visit(root_path)
+    expect(find('ul li')).to have_content('Título:')
+  end
 end
