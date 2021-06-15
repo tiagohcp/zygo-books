e8a99bd353345f30d0e7c5fb1958c0fbca67f505
Tiago Pereira <tiago@glin.com.br>
2021-06-14 21:32:18 -0300

    create admin books show

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -18,6 +18,19 @@ ActiveAdmin.register Book do
     actions
   end
 
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :author
+      row :image do |r|
+        if r.image?
+          image_tag url_for(r.image), :style => "width:100px;"
+        end
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :title
