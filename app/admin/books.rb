7ba0b1c348cd4789a9dd73bc44a0aaa4bcecc8de
Tiago Pereira <tiago@glin.com.br>
2021-06-14 21:29:30 -0300

    create admin books index

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,18 +1,16 @@
 ActiveAdmin.register Book do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :description, :author
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :description, :author]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  index do
+    selectable_column
+    column :title
+    column :description
+    column :author
+    column :image do |r|
+      if r.image?
+        image_tag url_for(r.image), :style => "width:100px;"
+      end
+    end
+    actions
+  end
   
 end
