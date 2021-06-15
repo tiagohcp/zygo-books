f9002a3d4b6fedebbe96731f720bcaae78c8107c
Tiago Pereira <tiago@glin.com.br>
2021-06-15 13:30:31 -0300

    test if index all books

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 feature "Books", type: :feature do
-  let!(:book) { Fabricate(:book) }
 
-  scenario 'Mostra mensagem - Bem-Vindo' do
+  scenario 'show message - Wellcome' do
     visit(root_path)
-    expect(page).to have_content('Bem-Vindo')
+    expect(page).to have_content('Wellcome')
   end
 
-  scenario 'Verificar exibição de livro' do
+  let!(:book) { Fabricate(:book) }
+
+  scenario 'should render a book' do
     visit(root_path)
     expect(page).to have_content(book.title)
     expect(page).to have_content(book.description)
     expect(page).to have_content(book.author)
+  end
+
+  let!(:book2) { Fabricate(:book) }
+  
+  scenario 'should render all books' do
+    visit(root_path)
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book2.title)
   end
 end
