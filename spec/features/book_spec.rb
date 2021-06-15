e0422bbc0a2a76a736c8ccfd18badf15a72871a7
Tiago Pereira <tiago@glin.com.br>
2021-06-15 14:36:41 -0300

    test shows filters label

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 feature "Books", type: :feature do
 
+  let!(:book) { Fabricate(:book) }
+  let!(:book2) { Fabricate(:book) }
+
   scenario 'show message - Wellcome' do
     visit(root_path)
     expect(page).to have_content('Wellcome')
   end
-
-  let!(:book) { Fabricate(:book) }
 
   scenario 'should render a book' do
     visit(root_path)
@@ -15,12 +16,16 @@ feature "Books", type: :feature do
     expect(page).to have_content(book.description)
     expect(page).to have_content(book.author)
   end
-
-  let!(:book2) { Fabricate(:book) }
   
   scenario 'should render all books' do
     visit(root_path)
     expect(page).to have_content(book.title)
     expect(page).to have_content(book2.title)
   end
+
+  scenario 'should have label - Filters'do
+    visit(root_path)
+    expect(page).to have_content('Filters')
+  end
+
 end
