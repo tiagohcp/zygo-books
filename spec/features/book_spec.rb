e00781220ffdcbee32b2c41319acd5b573626c56
Tiago Pereira <tiago@glin.com.br>
2021-06-15 10:19:51 -0300

    view showing a book

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 feature "Books", type: :feature do
+  let!(:book) { Fabricate(:book) }
+
   scenario 'Mostra mensagem - Bem-Vindo' do
     visit(root_path)
     expect(page).to have_content('Bem-Vindo')
@@ -8,6 +10,8 @@ feature "Books", type: :feature do
 
   scenario 'Verificar exibição de livro' do
     visit(root_path)
-    expect(find('ul li')).to have_content('Título:')
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.description)
+    expect(page).to have_content(book.author)
   end
 end
