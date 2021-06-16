2c046b6b8b982eaee08e0092abe4429075fb3d8b
Tiago Pereira <tiago@glin.com.br>
2021-06-15 22:45:29 -0300

    start filters partial to mobile

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'devise'
 gem 'activeadmin'
 
 gem 'ransack'
+
+gem 'browser', '~> 4.2.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  require "browser/aliases"
+
+  before_action :set_device_type
+
+  Browser::Base.include(Browser::Aliases)
+
+  def set_device_type
+    Rails.logger.info('*********** ENTROU set_device_type *************')
+    Rails.logger.info(browser)
+    Rails.logger.info(browser.mobile?)
+    request.variant = :phone if browser.mobile?
+  end
 end
