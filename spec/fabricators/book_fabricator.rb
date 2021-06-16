b56684f44337754d3fdfd5b1f81a25ddc7c90a43
Tiago Pereira <tiago@glin.com.br>
2021-06-16 19:24:44 -0300

    add image to book fabricator

--- spec/fabricators/book_fabricator.rb
+++ spec/fabricators/book_fabricator.rb
@@ -2,6 +2,10 @@ Fabricator(:book) do
   title { Faker::Book.title }
   description { Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 7) }
   author { Faker::Book.author }
-  # image { "#{Rails.root}/spec/fixtures/cover.png" }
-  # image(count: 1)
+  image {
+    Rack::Test::UploadedFile.new(
+      "./spec/support/files/noimageavailable.png",
+      "application/png"
+    )
+  }
 end
