5e974ca3c1117f542cd85f0630f679edf71673d9
Tiago Pereira <tiago@glin.com.br>
2021-06-16 17:18:33 -0300

    image required and create seeds to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :author, presence: true
+  validates :image, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 0..1.megabytes }
 
   def image?
     image.attached?
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if Book.count.zero?
+  10.times do
+    b = Book.create(
+      title: Faker::Book.title,
+      description: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 7),
+      author: Faker::Book.author,
+    )
+    b.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "noimageavailable.png")), filename: 'file.png')
+    b.save
+  end
+end
